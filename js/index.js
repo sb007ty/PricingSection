3936ae478cd9a4e9f905366ffd4a0341552522cc
spandan <spandan7b@gmail.com>
2024-09-25 20:18:02 +0530

    final-change

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const btn = document.querySelector(".button-sec");
+const card = document.querySelector(".cards-container");
+const allCards = document.querySelectorAll(".card");
+
+// btn.addEventListener("click", clickBtn);
+card.addEventListener("click", clickCard);
+
+function clickCard(e) {
+  const clickedNode = e.target;
+  const parentCard = clickedNode.closest(".card");
+  if (!parentCard) return;
+  for (let i = 0; i < allCards.length; i++) {
+    let card = allCards.item(i);
+    card.classList.remove("card-active");
+  }
+  parentCard.classList.add("card-active");
+  //   console.log(parent);
+}
